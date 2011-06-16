115117c88f5980d79a81df23097a89ac8ff9c54c
Akihiro Maeda <sola.1980.a@gmail.com>
2011-06-17 00:59:09 +0900

    Android の変更をマージしたのみ（動かない）

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -27,6 +27,9 @@
 #include "power.h"
 
 const char *const pm_states[PM_SUSPEND_MAX] = {
+#ifdef CONFIG_EARLYSUSPEND
+	[PM_SUSPEND_ON]		= "on",
+#endif
 	[PM_SUSPEND_STANDBY]	= "standby",
 	[PM_SUSPEND_MEM]	= "mem",
 };
@@ -209,6 +212,7 @@ int suspend_devices_and_enter(suspend_state_t state)
 			goto Close;
 	}
 	suspend_console();
+	pm_restrict_gfp_mask();
 	suspend_test_start();
 	error = dpm_suspend_start(PMSG_SUSPEND);
 	if (error) {
@@ -225,6 +229,7 @@ int suspend_devices_and_enter(suspend_state_t state)
 	suspend_test_start();
 	dpm_resume_end(PMSG_RESUME);
 	suspend_test_finish("resume devices");
+	pm_restore_gfp_mask();
 	resume_console();
  Close:
 	if (suspend_ops->end)
